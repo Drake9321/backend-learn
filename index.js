bcd3e8b0d0dd7c1dca7aebd37e5a86c4706d0953
Mayank Srivastava <mayank21srivastava@gmail.com>
2026-07-22 16:00:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import http from 'http';
+
+const port =5000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+server.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
