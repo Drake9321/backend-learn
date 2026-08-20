1af5fc34cd8e57c4925ae15a717a16f8b655a78d
Nathan Drake <drakennathan2049@protonmail.com>
2026-08-20 23:05:11 +0530

    Changed CreateUser method

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,28 +50,11 @@ const deleteUser = async (req, res) => {
 
 const createUser = async (req, res) => {
   try {
-    console.log("Request body:", req.body);
-
     const newUser = new User(req.body);
-
     const savedUser = await newUser.save();
-
-    res.status(201).json({
-      message: "User created successfully",
-      data: savedUser
-    });
+    res.status(201).send({ message: "User created successfully", data: savedUser });
   } catch (error) {
-    console.error("CREATE USER ERROR:", error);
-
-    res.status(400).json({
-      message: error.message,
-      errors: error.errors
-        ? Object.values(error.errors).map((err) => ({
-            field: err.path,
-            message: err.message
-          }))
-        : undefined
-    });
+    res.status(400).send(error);
   }
 };
 
